e527f04b673d322a2d15e6504be8a373dad5c073
Estebahn Molina <molina.estebahn@gmail.com>
2022-03-24 16:06:12 -0500

    complete syntax variables and types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,43 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+    int myFavoriteNumber= 59;
+        System.out.println("myFavoriteNumber = " + myFavoriteNumber);
+    String  myString   ="My First Java String";
+//  String myString='m';
+//  String myString=3.14159
+
+        System.out.println("myString = " + myString);
+        double myNumber= 3.14;
+        System.out.println("myNumber = " + myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int class
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+//        int x = 4;
+//        x += 5;
+//        System.out.println("x = " + x);
+
+//        int x = 3;
+//        int y = 4;
+//       y *= x;
+//        System.out.println("y = " + y);
+
+        int x = 10;
+        int y = 2;
+        x /=   y;
+        y -=   x;
+        System.out.println("y = " + y);
+        System.out.println("x = " + x);
+
     }
 }
