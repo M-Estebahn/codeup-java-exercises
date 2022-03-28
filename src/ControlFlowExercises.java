f795b06c538a30db586fee6a69ffe90dc5837054
Estebahn Molina <molina.estebahn@gmail.com>
2022-03-28 16:29:30 -0500

    complete FizzBuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -13,12 +13,25 @@ public class ControlFlowExercises {
 //            start*=start ;
 //        } while (start <= 1000000);
 
-        for (int i=5 ; i<=15 ; i++){
-            System.out.println(i);
-        }
-        for (long  i=2;i<=100000000; i*=i){
-            System.out.println(i);
-        }
+//        for (int i=5 ; i<=15 ; i++){
+//            System.out.println(i);
+//        }
+//        for (long  i=2;i<=100000000; i*=i){
+//            System.out.println(i);
+//        }
+        for(int i=0; i<=100;++i){
+            if(i % 3 ==0 && i % 5 ==0) {
+            System.out.println("FizzBuzz");
+                continue;
+        }if (i%3 == 0){
+             System.out.println("Fizz");
+                continue;
+            }if (i%5==0){
+                System.out.println("Buzz");
 
+            }else{
+                System.out.println(i);
+            }
+        }
     }
 }
