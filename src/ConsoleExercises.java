7f9ab658cf69c54be6194746903f6cb29118a354
Estebahn Molina <molina.estebahn@gmail.com>
2022-03-28 15:50:47 -0500

    modify Console Exercises to accept 3 words at once, create file for Control Flow exercises and complete first while loop

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -13,30 +13,36 @@ public class ConsoleExercises {
 
         double pi = 3.14159;
 
-        System.out.printf("The value of pi is approximately; %.2f%n", pi);
+        System.out.printf("The value of pi is approximately: %.2f %n", pi);
+
         System.out.println("enter an integer");
         int userInt=sc.nextInt();
         System.out.println("you entered: "+userInt);
-        System.out.println("enter a word");
+
+        System.out.println("enter 3 words");
         String str1= sc.next();
-        System.out.println("enter a second word");
         String str2=sc.next();
-        System.out.println("enter a third word");
         String str3= sc.next();
+
         System.out.println(str1);
         System.out.println(str2);
         System.out.println(str3);
+
         System.out.println("enter a sentence");
         String sent=sc.next();
         System.out.println(sent);
+
         System.out.println("Enter a sentence again");
         sc.nextLine();
         String sentence=sc.nextLine();
         System.out.println(sentence);
+
         System.out.println("enter the width");
         int width= sc.nextInt();
+
         System.out.println("enter the length");
         int length=sc.nextInt();
+
         System.out.println("The Area is:"+(length*width));
         System.out.println("The Perimeter is:"+((length*2)+(width*2)));
 
