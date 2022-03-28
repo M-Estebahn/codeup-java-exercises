20ed1302e0cbbef6f9a4887ce72b98bd45167709
Estebahn Molina <molina.estebahn@gmail.com>
2022-03-28 16:15:42 -0500

    complete refactor to for loop exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,18 +1,24 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//       long start = 2;
+//
+//        do {
+//            System.out.println(start);
+//            start*=start ;
+//        } while (start <= 1000000);
+
+        for (int i=5 ; i<=15 ; i++){
             System.out.println(i);
-            i++;
         }
-
-       long start = 2;
-
-        do {
-            System.out.println(start);
-            start*=start ;
-        } while (start <= 1000000);
-
+        for (long  i=2;i<=100000000; i*=i){
+            System.out.println(i);
+        }
 
     }
 }
