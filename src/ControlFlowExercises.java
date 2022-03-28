3535bc506eab4d122c518fe3273a6cef7ea6266b
Estebahn Molina <molina.estebahn@gmail.com>
2022-03-28 15:57:57 -0500

    complete count by 2s exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,18 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i=5;
-        while (i<=15){
+        int i = 5;
+        while (i <= 15) {
             System.out.println(i);
             i++;
         }
+
+        int start = 0;
+
+        do {
+            System.out.println(start);
+            start += 2;
+        } while (start <= 100);
+
+
     }
 }
