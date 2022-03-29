2f58943c670d84210220ed50bab1451fe3492170
Estebahn Molina <molina.estebahn@gmail.com>
2022-03-29 10:15:41 -0500

    complete last two exercises for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
 //        int i = 5;
 //        while (i <= 15) {
 //            System.out.println(i);
@@ -33,5 +36,76 @@ public class ControlFlowExercises {
                 System.out.println(i);
             }
         }
+        // Welcome the user (code from https://stackoverflow.com/questions/22441530/java-powers-display-table)
+        System.out.println("Welcome to the Squares and Cubes table");
+        System.out.println();
+
+
+        String choice = "y";
+
+        do
+        {
+            // Get input from the user
+            System.out.print("Enter an integer: ");
+            int integer = sc.nextInt();
+
+            // Create a header
+            String header = "Number  " + "Squared " + "Cubed   " + "\n"
+                    +   "======  " + "======= " + "=====   ";
+            System.out.println(header);
+
+            int square = 0;
+            int cube = 0;
+
+            String row = "";
+            for (int i = 1; i <= integer; i++)
+            {
+
+                square = i * i;
+                cube = i * i * i;
+
+                row = i + "       " + square + "       " + cube;
+                System.out.println(row);
+            }
+
+            // See if the user wants to continue
+            System.out.print("Continue? (y/n): ");
+            choice = sc.next();
+            System.out.println();
+
+        }
+        while (!choice.equalsIgnoreCase("n"));
+
+        String proceed="y";
+
+        do{
+            System.out.println("Enter a numerical grade value");
+            int grade=sc.nextInt();
+
+            if (grade<= 100 && grade>=88){
+                System.out.println("A");
+            }
+            if (grade<= 87 && grade>=80){
+                System.out.println("B");
+            }
+            if (grade<= 79 && grade>=67){
+                System.out.println("C");
+            }
+            if (grade<= 66 && grade>=60){
+                System.out.println("D");
+            }
+            if (grade<= 59 && grade>=0){
+                System.out.println("F");
+            }
+
+
+
+            // See if the user wants to continue
+            System.out.print("Continue? (y/n): ");
+            choice = sc.next();
+            System.out.println();
+
+        }
+        while (!choice.equalsIgnoreCase("n"));
     }
 }
