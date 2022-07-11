07806f9d7abae68d62bdb25bf0eb70d0be099467
M-Estebahn <molina.estebahn@gmail.com>
2022-07-11 17:48:42 -0500

    java fizzbuzz

---
+++ src/PrintFizzBuzz.java
@@ -0,0 +1,19 @@
+public class PrintFizzBuzz {
+     public static void main(String[] args) {
+        fizzbuzz(100);
+    }
+    public static void fizzbuzz(int n){
+         for(int i=1 ;i<=n;i++){
+             if (i % 3 == 0 && i % 5 ==0){
+                 System.out.println("fizzbuzz");
+             }else if(i % 3 == 0){
+                 System.out.println("fizz");
+             }else if (i % 5 ==0){
+                 System.out.println("buzz");
+             }else{
+                 System.out.println(i);
+             }
+         }
+    }
+
+}
